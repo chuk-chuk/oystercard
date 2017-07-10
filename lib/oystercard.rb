c11099b8cf1b1344cda42fb6133ab2a83fe9f6fa
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-07-10 16:46:47 +0100

    challenge 6: raise an error when the topup limit is exceeded

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,13 +3,20 @@ class Oystercard
   attr_reader :balance
 
   DEFAULT_BALANCE = 0
+  LIMIT = 90
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
 
   def top_up(topup_value)
+    raise "Sorry, the maximum amount is £#{LIMIT}" if exceeded?(topup_value)
      @balance += topup_value
   end
 
+  private
+
+  def exceeded?(topup_value)
+    @balance + topup_value > LIMIT
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,12 +2,14 @@ require 'oystercard'
 
 describe Oystercard do
 
+  let(:bad_topup) { 120 }
   let(:default_balance) { 0 }
   let(:topup_value) {50}
-   subject(:oystercard) { described_class.new }
+  let(:max_limit) { Oystercard::LIMIT }
+  subject(:oystercard) { described_class.new }
 
-   it { is_expected.to respond_to(:balance) }
-   it { is_expected.to respond_to(:top_up) }
+  it { is_expected.to respond_to(:balance) }
+  it { is_expected.to respond_to(:top_up) }
 
   it "has a default balance of 0" do
     expect(oystercard.balance).to eq default_balance
@@ -15,6 +17,10 @@ describe Oystercard do
 
   it "allows a card to be topped up" do
     expect { oystercard.top_up(topup_value) }.to change{ oystercard.balance }.by topup_value
+  end
+
+  it "raises an error if the card is topped up over the maximum limit" do
+    expect { oystercard.top_up(bad_topup) }.to raise_error("Sorry, the maximum amount is £#{ max_limit }")
   end
 
 end
