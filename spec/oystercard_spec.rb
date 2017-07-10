03529671a6efa6c4029bf671a2b2313f6b42b2a4
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-07-10 15:12:27 +0100

    challenge 4: created test for default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,14 @@
+require 'oystercard'
 
 describe Oystercard do
-  
+
+  # let(:balance) { 200 }
+  let(:default_balance) { 0 }
+   subject(:oystercard) { described_class.new }
+
+   it { is_expected.to respond_to(:balance) }
+
+  it "has a default balance of 0" do
+    expect(oystercard.balance).to eq default_balance
+  end
 end
